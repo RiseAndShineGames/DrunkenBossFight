d52ead7d9923e9430247e35cdb1ce2a4366e21a7
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-01-23 15:22:51 -0500

    add playerhit script

---
+++ src/systems/simulation/player-hit.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
+	ecs.addEach(function (entity, elapsed) { // eslint-disable-line no-unused-vars
+		var entityCollisions = data.entities.get(entity, "collisions");
+		var playerHealth = data.entities.get(entity, "health");
+		var damage = 5;
+
+		for (var i = 0; i < entityCollisions.length; i++) {
+			var other = entityCollisions[i];
+			if (data.entities.get(other, "Flame")) {
+				data.entities.destroy(other);
+				data.entities.set(entity, "health", playerHealth - damage);
+			}
+		}
+	}, "player");
+};

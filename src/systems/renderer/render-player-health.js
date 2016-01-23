dd72f0bd6f84932d5c53ff62b45f2b3bc367711c
justvcreative <justvcreative@gmail.com>
2016-01-23 15:44:18 -0500

    changed sprites & fixed sobriety meter

--- src/systems/renderer/render-player-health.js
+++ src/systems/renderer/render-player-health.js
@@ -5,9 +5,9 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 		context.fillStyle = "#f7931e";
 		var position = data.entities.get(5,"position");
 		var height = 50 * data.entities.get(1,"health")/data.entities.get(1,"maxHealth");
-		var healthx = position.x + 15;
+		var healthx = position.x + 23;
 		var healthy = position.y + 62 - height;
-		var healthWidth = 38;
+		var healthWidth = 42;
 		context.fillRect(healthx,healthy, healthWidth, height);
 		
 	});

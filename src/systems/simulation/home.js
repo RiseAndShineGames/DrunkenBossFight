2f6a7326da44eb2ca2512e113a8940e664c2fa03
justvcreative <justvcreative@gmail.com>
2016-02-22 19:55:52 -0500

    Added More Buttons to Game Over

---
+++ src/systems/simulation/home.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = function(ecs, data) {
+	
+	ecs.addEach(function(entity, elapsed) { // jshint ignore:line
+	var collisions = data.entities.get(entity, "collisions");
+	var cursor = 300;
+	for ( var i = 0; i < collisions.length; i++ ) {
+		if (collisions[i] == cursor) {
+			if(data.input.mouse.consumePressed(0)){
+				data.switchScene("title");
+			}
+			var image = data.entities.get(entity, "image");
+			image.sourceX = 320;
+			return ;
+		}
+	}	
+			var image = data.entities.get(entity, "image");
+			image.sourceX = 0;
+	},"home_btn");
+};
